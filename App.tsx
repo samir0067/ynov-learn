6d4c86d99abf9c56b5e7048fa0bc18426205d9c9
samir <arrahmani.samir@gmail.com>
2026-04-07 11:24:38 +0200

    feat/navigation screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
 import HomeScreen from './src/screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import DetailScreen from './src/screens/DetailScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import DemoScreens from './src/screens/DemoScreens';
+import DetailScreen from './src/screens/DetailScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} options={{title: "Accueil" }} />
-      <Stack.Screen name="Detail" component={DetailScreen} options={ {title: "Détail" } } />
-      <Stack.Screen name="Demo" component={DemoScreens} options={ {title: "Démonstration" } } />
-    </Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name='Home' component={HomeScreen} options={{ title: "Accueil" }} />
+        <Stack.Screen name='Detail' component={DetailScreen} options={{ title: "Détail" }} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
