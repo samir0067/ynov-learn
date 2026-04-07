5dd9f5f31c3081ad9a0213edde0823d7e0c5fcd7
enz0r68 <enzoragni18@gmail.com>
2026-04-07 12:12:31 +0200

    base mobil

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,47 +1,39 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet} from "react-native";
 import Button from "../components/Button";
 import { COLORS } from "../constants/colors";
 
 type Props = {
-  navigation: any 
+    navigation: any;
 }
+
 
 export default function HomeScreen({navigation}: Props) {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Bienvenue 🏠</Text>
-            <Text style={styles.subtitle}>Ceci est l'écran d'accueil</Text>
-            <Button label="Aller au détail" onPress={() => navigation.navigate("Detail")} />
-            <Button 
-                label="Voir la page démo" 
-                color={COLORS.secondary}
-                onPress={() => navigation.navigate("Demo")} 
-            />
-            <Button
-                label="📱 Exercice : Profils" 
-                onPress={() => navigation.navigate("Profile")} 
-                color={COLORS.gray}
-            />
+        <View>
+            <Text>Accueil 🏠</Text>
+            <Button label="Confirmer" onPress={() => undefined} />
+            <Button label="Voir le détail" onPress={() => navigation.navigate("Detail")} color={COLORS.primary} />
+            <Button label="Voir la démonstration" onPress={() => navigation.navigate("Demo")} color={COLORS.secondary} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex : 1,
-        justifyContent: "center",
-        alignItems: "center",
+        flex: 1,
         backgroundColor: COLORS.background,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     title: {
         fontSize: 28,
-        fontWeight: "bold",
-        color: COLORS.black,
+        fontWeight: 'bold',
         marginBottom: 8,
+        color: COLORS.black,
     },
-    subtitle: {
+    subtitle: {        
         fontSize: 16,
         color: COLORS.gray,
         marginBottom: 32,
-    }
-})
+    },
+});
