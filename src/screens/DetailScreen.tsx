ced65203ad3cfb0576e15a0941749ff9b2437f5c
samir <arrahmani.samir@gmail.com>
2026-04-07 11:24:38 +0200

    feat/navigation screens

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -2,19 +2,20 @@ import { StyleSheet, Text, View } from "react-native";
 import Button from "../components/Button";
 import { COLORS } from "../constants/colors";
 
-type DetailScreenProps = {
-    navigation: any;
+type Props = {
+  navigation: any 
 }
 
-export default function DetailScreen({navigation}: DetailScreenProps) {
+export default function DetailScreen({navigation}: Props) {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Détail 📄</Text>
+            <Text style={styles.subtitle}>Vous êtes sur l'écran de détail</Text>
             <Button 
               label="← Retour" 
               onPress={() => navigation.goBack()} 
               color={COLORS.secondary} 
-              />
+            />
         </View>  
     )
 }
@@ -27,15 +28,15 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.background,
   },
   title: {
-    fontSize: 24,
+    fontSize: 28,
     fontWeight: 'bold',
     color: COLORS.black,
-    marginBottom: 20,
+    marginBottom: 8,
   },
-  subtitle: {        
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: COLORS.gray,
-        marginBottom: 32,
-    },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: COLORS.gray,
+    marginBottom: 32,
+  }
 });
