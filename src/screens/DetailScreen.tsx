cbc7f811137ca594088ec2949012f89a0bda432d
unknown <julie.lefebvre@ynov.com>
2026-04-07 11:25:29 +0200

    07am

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -2,16 +2,20 @@ import { StyleSheet, Text, View } from "react-native";
 import Button from "../components/Button";
 import { COLORS } from "../constants/colors";
 
-type DetailScreenProps = {
-    onBack: () => void
+type Props = {
+  navigation: any
 }
 
-export default function DetailScreen({onBack}: DetailScreenProps) {
+export default function DetailScreen({navigation}: Props) {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Détail 📄</Text>
-            <Button label="← Retour" onPress={onBack} color={COLORS.secondary} />
-        </View>  
+            <Text style={styles.subtitle}>Ceci est l'écran de détail</Text>
+            <Button
+              label="← Retour"
+              onPress={() => navigation.goBack()}
+              color={COLORS.secondary} />
+        </View>
     )
 }
 
@@ -23,6 +27,12 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.background,
   },
   title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: COLORS.black,
+    marginBottom: 8,
+  },
+  subtitle: {
     fontSize: 24,
     fontWeight: 'bold',
     color: COLORS.black,
