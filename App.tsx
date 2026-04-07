f0799520d839ead6b64349b1ce008ccd6351a69f
bierl <loic.bier@ynov.com>
2026-04-07 11:26:53 +0200

    detail et accueil

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import DetailScreen from './src/screens/DetailScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <HomeScreen/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Accueil" }} />
+        <Stack.Screen name="Detail" component={DetailScreen} options={{ title: "Détail" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -3,29 +3,33 @@ import Button from "../components/Button";
 import { COLORS } from "../constants/colors";
 
 type DetailScreenProps = {
-    onBack: () => void
-}
+    navigation: any;
+};
 
-export default function DetailScreen({onBack}: DetailScreenProps) {
+export default function DetailScreen({ navigation }: DetailScreenProps) {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Détail 📄</Text>
-            <Button label="← Retour" onPress={onBack} color={COLORS.secondary} />
-        </View>  
-    )
+            <Button
+                label="← Retour"
+                onPress={() => navigation.goBack()}
+                color={COLORS.secondary}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: COLORS.background,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: COLORS.black,
-    marginBottom: 20,
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: COLORS.background,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: COLORS.black,
+        marginBottom: 20,
+    },
 });
