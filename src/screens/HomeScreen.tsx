6d8215c199bbd3199adfe10cbefb5093c34a34af
samir <arrahmani.samir@gmail.com>
2026-04-07 12:17:11 +0200

    créneau 2 : démo + exercice profils

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -12,6 +12,16 @@ export default function HomeScreen({navigation}: Props) {
             <Text style={styles.title}>Bienvenue 🏠</Text>
             <Text style={styles.subtitle}>Ceci est l'écran d'accueil</Text>
             <Button label="Aller au détail" onPress={() => navigation.navigate("Detail")} />
+            <Button 
+                label="Voir la page démo" 
+                color={COLORS.secondary}
+                onPress={() => navigation.navigate("Demo")} 
+            />
+            <Button
+                label="📱 Exercice : Profils" 
+                onPress={() => navigation.navigate("Profile")} 
+                color={COLORS.gray}
+            />
         </View>
     )
 }
