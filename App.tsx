b03867e3378ffa862827296d32a423fece3cc813
unknown <julie.lefebvre@ynov.com>
2026-04-08 23:31:05 +0200

    08-pm

--- App.tsx
+++ App.tsx
@@ -11,16 +11,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-<<<<<<< HEAD
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Accueil" }} />
-        <Stack.Screen name="Detail" component={DetailScreen} options={{ title: "Détail" }} />
-        <Stack.Screen name="Demo" component={DemoScreen} options={{ title: "Démonstration" }} />
-=======
         <Stack.Screen name='Home' component={HomeScreen} options={{ title: "Accueil" }} />
         <Stack.Screen name='Detail' component={DetailScreen} options={{ title: "Détail" }} />
         <Stack.Screen name='Demo' component={DemoScreen} options={{ title: "Démo composants" }} />
         <Stack.Screen name='Profile' component={ProfileScreen} options={{ title: "Profils" }} />
->>>>>>> 26455d71d8ce5a17143eee1f59734e24dfbdf16d
       </Stack.Navigator>
     </NavigationContainer>
   );
